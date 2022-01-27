befbdb0f0f202e562662bbffc881c682aaaad278
CodingWon <kingcoding2298@gmail.com>
2022-01-27 16:53:00 +0900

    plus 완성

--- JavaScript/newLect/calculator/calculator.js
+++ JavaScript/newLect/calculator/calculator.js
@@ -3,18 +3,55 @@ window.addEventListener("load",function(){
     var calcdiv = section.querySelector("div");
     var txtInput = section.querySelector(".text-area");
     var plusInput = section.querySelector(".plus");
-    
+  
+    var resultInput = section.querySelector(".result");
+    var plus = 0;
+    var channel = 0;
+ 
 
     calcdiv.onclick = function(e){
         e.preventDefault();
+
         if(e.target.nodeName != "INPUT")
             return;
-       txtInput.value += e.target.value;
+
+        if(txtInput.value == "0")
+            txtInput.value="";
+
+            var value =  txtInput.value += e.target.value;
+        }
+
+    plusInput.onclick = function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        
+        plus += parseInt(txtInput.value);
+        txtInput.value = 0;
+
+        console.log(plus);
+        channel = 1;
     }
 
     plusInput.onclick = function(e){
         e.preventDefault();
+        e.stopPropagation();
         
+        plus += parseInt(txtInput.value);
+        txtInput.value = 0;
+        
+        console.log(plus);
+        channel = 1;
+    }
+
+
+    resultInput.onclick = function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        if(channel == 1){
+
+            plus += parseInt(txtInput.value);
+            txtInput.value = plus;
+        }
     }
 
 
