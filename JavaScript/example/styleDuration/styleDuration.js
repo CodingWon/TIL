9d4ddaa92640d1fc48efd6f11d31fc4a66fcee15
CodingWon <kingcoding2298@gmail.com>
2022-02-02 11:50:50 +0900

    js예제 :  styleDuration

---
+++ JavaScript/example/styleDuration/styleDuration.js
@@ -0,0 +1,31 @@
+window.addEventListener("load",function(){
+    var section = document.querySelector("#container");
+    var styleInput = section.querySelector(".style-input");
+    var widthInput = section.querySelector(".width-input");
+    var radiusInput = section.querySelector(".radius-input");
+    var colorInput = section.querySelector(".color-input");
+    var borderColorInput = section.querySelector(".bordercolor-input");
+    var item = section.querySelector(".item");
+
+
+    styleInput.oninput = function(){
+        item.style.borderStyle = styleInput.value;
+    }
+
+    widthInput.oninput = function(){
+        item.style.borderWidth = widthInput.value+"px";    
+    }
+
+    radiusInput.oninput = function(){
+        item.style.borderRadius = radiusInput.value+"px";
+    }
+
+    colorInput.oninput = function(){
+        item.style.backgroundColor = colorInput.value;
+    }
+
+    borderColorInput.oninput = function(){
+        item.style.borderColor = borderColorInput.value;
+    }
+
+});
