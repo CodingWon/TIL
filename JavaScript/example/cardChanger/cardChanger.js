dbedceb07d859dd16235fe5ead49c24601c66161
CodingWon <kingcoding2298@gmail.com>
2022-02-03 14:58:17 +0900

    js 예제 : cardChanger - 완성

--- JavaScript/example/cardChanger/cardChanger.js
+++ JavaScript/example/cardChanger/cardChanger.js
@@ -4,12 +4,15 @@ window.addEventListener("load",function(){
     var btnNext = section.querySelector(".btn-next");
     var lis = section.querySelectorAll("li");
 
+    var offIndex = 0;
     btnNext.onclick = function(e){
         e.preventDefault();
 
-        lis[0].className = "card-3th";
-        lis[1].className = "card-1th";
-        lis[2].className = "card-2th";
+        offIndex++;
+
+        lis[(0+offIndex)%3].className = "card-1th";
+        lis[(1+offIndex)%3].className = "card-2th";
+        lis[(2+offIndex)%3].className = "card-3th";
     };
     
 });
