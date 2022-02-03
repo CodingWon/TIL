d77dafa619431f4a563494b04e92919d46b75325
CodingWon <kingcoding2298@gmail.com>
2022-02-03 11:14:32 +0900

    js 예제 : accordion js 완성

--- JavaScript/example/accordionMenu/accordionMenu.js
+++ JavaScript/example/accordionMenu/accordionMenu.js
@@ -1,11 +1,23 @@
 window.addEventListener("load",function(){
     var section = document.querySelector("#s6");
     var box = section.querySelector(".accordion-box");
+    var current = section.querySelector(".active");
 
     box.onclick = function(e){
 
-        if(!e.target.classList.contains("accordion-box"))
+        var valid = e.target.nodeName =="H2"
+                    || e.target.classList.contains("accordion-header");
+
+     
+
+        if(!valid)
             return;
-    }
+
+        if(current != null)
+            current.classList.remove("active")
+    
+        e.target.classList.add("active");
+        current = e.target;
+    };
 
 });
